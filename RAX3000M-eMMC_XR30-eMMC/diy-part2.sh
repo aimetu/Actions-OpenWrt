d96934a1fa2469d4bb7182f1d7b7173d2f649ffc
aimetu <41498149+aimetu@users.noreply.github.com>
2024-06-13 09:21:01 +0800

    Update diy-part2.sh

--- RAX3000M-eMMC_XR30-eMMC/diy-part2.sh
+++ RAX3000M-eMMC_XR30-eMMC/diy-part2.sh
@@ -10,16 +10,26 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-##-----------------Add OpenClash dev core------------------
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-##-----------------Delete DDNS's examples-----------------
-sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
-##-----------------Manually set CPU frequency for MT7981B-----------------
-sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+# 修改 device 设备名称
+# sed -i "s/LibWrt/OpenWrt/g" package/base-files/files/bin/config_generate
+
+# 默认网关 ip 地址修改
+sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/files/bin/config_generate
+
+# 修改 wifi 无线名称
+# sed -i "s/LibWrt/OpenWrt/g" package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+
+# 更换 5.4 内核为 5.10 内核
+# sed -i "s/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g" target/linux/ramips/Makefile
+
+# 取消 bootstrap 为默认主题，将 argon 设置为默认主题
+rm -rf feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+
+# 添加 AdguardHome 广告过滤插件，删除自带 AdguardHome 文件
+rm -rf feeds/packages/net/adguardhome
+# git clone https://github.com/kongfl888/luci-app-adguardhome package/new/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/new/luci-app-adguardhome
