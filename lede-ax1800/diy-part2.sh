f208149ca9be752d89f8ab52e9b94f23972248a2
aimetu <41498149+aimetu@users.noreply.github.com>
2024-11-03 14:00:34 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -35,6 +35,17 @@ rm -rf feeds/packages/net/adguardhome
 # git clone https://github.com/kongfl888/luci-app-adguardhome package/new/luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/new/luci-app-adguardhome
 
+# 更新 lean 的内置的 smartdns 版本
+rm -rf feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=e89778809897329f48c662b5cb9d69ed9df6c032" feeds/packages/net/smartdns/Makefile
+sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
+
+# 安装 luci-app-smartdns（ 必选 )
+rm -rf feeds/luci/applications/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/pymumu/smartdns.git package/smartdns
+
 # 插件 mosdns & alist 依赖，删除重复 golang & v2ray 防止插件冲突
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
