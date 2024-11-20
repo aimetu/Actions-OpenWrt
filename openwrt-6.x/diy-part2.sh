83ccb3bbaa9a7a436ae0811b8f0eb744882a7f04
aimetu <41498149+aimetu@users.noreply.github.com>
2024-11-20 10:28:31 +0800

    Update diy-part2.sh

--- openwrt-6.x/diy-part2.sh
+++ openwrt-6.x/diy-part2.sh
@@ -38,10 +38,10 @@ rm -rf feeds/packages/net/adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/new/luci-app-adguardhome
 
 # 插件 mosdns & alist 依赖，删除重复 golang & v2ray 防止插件冲突 
-# rm -rf feeds/packages/lang/golang
-# git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # rm -rf feeds/luci/applications/luci-app-mosdns
-# rm -rf feeds/packages/net/v2ray-geodata
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
