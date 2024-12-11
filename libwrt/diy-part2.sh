66ff8a072834ae2eb5ae620b0412b3f9c4026697
aimetu <41498149+aimetu@users.noreply.github.com>
2024-12-11 23:44:17 +0800

    更新 diy-part2.sh

--- libwrt/diy-part2.sh
+++ libwrt/diy-part2.sh
@@ -41,10 +41,10 @@ rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 
 # 添加 nekobox & mihomo 插件
