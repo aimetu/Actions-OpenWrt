a56ce4ad27f0fedcb02b763f8166c65729241034
aimetu <41498149+aimetu@users.noreply.github.com>
2024-12-16 18:32:45 +0800

    Update diy-part2.sh

--- lede-ax1800/diy-part2.sh
+++ lede-ax1800/diy-part2.sh
@@ -29,12 +29,12 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/invalid users = root/#&/g' feeds/packages/net/samba4/files/smb.conf.template
 
 # 取消 bootstrap 为默认主题，将 argon 设置为默认主题 ( for LuCI 18.06 & 23.05 )
-rm -rf feeds/luci/themes/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# rm -rf feeds/luci/themes/luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 # git clone  https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 # 添加 AdguardHome 广告过滤插件，删除自带 AdguardHome 文件
 rm -rf feeds/packages/net/adguardhome
@@ -42,20 +42,23 @@ rm -rf feeds/packages/net/adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/new/luci-app-adguardhome
 
 # 更新 lede 的内置的 smartdns 版本
-rm -rf feeds/packages/net/smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=e89778809897329f48c662b5cb9d69ed9df6c032" feeds/packages/net/smartdns/Makefile
-sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
+#rm -rf feeds/packages/net/smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+#sed -i "/^PKG_SOURCE_VERSION:=/cPKG_SOURCE_VERSION:=e89778809897329f48c662b5cb9d69ed9df6c032" feeds/packages/net/smartdns/Makefile
+#sed -i "s/PKG_MIRROR_HASH/#PKG_MIRROR_HASH/" feeds/packages/net/smartdns/Makefile
 
 # 安装 luci-app-smartdns（ 必选 )
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/luci/applications/luci-app-smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 # git clone https://github.com/pymumu/smartdns.git package/smartdns
 
-# 插件 mosdns & alist 依赖，删除重复 golang & v2ray 防止插件冲突
+# 更新 golang 依赖（ mosdns & alist 插件 )
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-# rm -rf feeds/luci/applications/luci-app-mosdns
+
+# 添加 mosdns 插件，删除重复 mosdns 文件
+rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
